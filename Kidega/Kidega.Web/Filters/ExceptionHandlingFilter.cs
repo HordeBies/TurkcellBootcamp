0b847ad1bc1abeea191f0a6296711080115ab5a5
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-06-10 12:18:10 +0300

    Updated Layout to authenticate some navbar buttons, updated exception handling filter to return home with friendly error message as popup

--- Kidega/Kidega.Web/Filters/ExceptionHandlingFilter.cs
+++ Kidega/Kidega.Web/Filters/ExceptionHandlingFilter.cs
@@ -1,30 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Kidega.Web.Filters
 {
-    public class ExceptionHandlingFilter : IExceptionFilter
+    public class ExceptionHandlingFilter(ILogger<ExceptionHandlingFilter> logger, LinkGenerator linkGenerator, ITempDataDictionaryFactory tempDataFactory) : IExceptionFilter
     {
-        private readonly ILogger<ExceptionHandlingFilter> _logger;
-
-        public ExceptionHandlingFilter(ILogger<ExceptionHandlingFilter> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnException(ExceptionContext context)
         {
             // Log the exception
-            _logger.LogError(context.Exception, "Unhandled exception occurred");
+            logger.LogError(context.Exception, "Unhandled exception occurred");
 
-            // Set the result to an error response
-            var result = new ObjectResult("An error occurred")
-            {
-                StatusCode = 500
-            };
+            // One option is to show an error page but i would prefer redirecting to home page
+            //var result = new ObjectResult("An error occurred")
+            //{
+            //    StatusCode = 500
+            //};
 
-            context.Result = result;
+            // Clear the response
+            context.Result = new EmptyResult();
             context.ExceptionHandled = true;
+
+            // Redirect to home index page
+            var homeIndexUrl = linkGenerator.GetPathByAction("Index", "Home");
+            context.HttpContext.Response.Redirect(homeIndexUrl);
+
+            // Add error message to TempData to show it on the home page
+            tempDataFactory.GetTempData(context.HttpContext).Add("error", context.Exception.Message);
         }
     }
 
